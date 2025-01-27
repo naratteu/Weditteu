4cc7137bbb8b37a379c6038ced733c8ece733862
LAPTOP-5VCKOCNQ\user <skfhddlg@vascoenergy.com>
2025-01-27 12:37:47 +0900

    first deployment

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using PuppeteerSharp;
+
+var fetch = new BrowserFetcher().DownloadAsync();
+
+string path = args[0];
+JsonNode? wdit = null;
+await using (var stream = File.OpenRead(path))
+{
+    var jdoc = await JsonDocument.ParseAsync(stream);
+    if (jdoc.RootElement.TryGetProperty("$wdit", out var w))
+        wdit = w.Deserialize<JsonNode>();
+    wdit ??= new JsonObject();
+}
+var obj = wdit switch
+{
+    JsonObject o => o,
+    JsonValue v => new() { ["--app"] = v },
+};
+await fetch;
+/*await using*/var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = false,
+    Args = [.. obj.Select(kv => $"{kv.Key}={kv.Value}")]
+});
+
+var pages = await browser.PagesAsync();
+var page = pages.Single();
+
+var fileInput = await page.WaitForSelectorAsync("input.weditteu");
+await Task.Delay(500);
+await fileInput.UploadFileAsync(path);
---
+++ README.md
@@ -0,0 +1,19 @@
+# WEb EDITor by naraTTEU for local files
+
+## 이게 뭡니까?
+
+로컬파일을 읽거나 편집하는 과정에서 NativeGUI를 배제하고 웹에 의존하기위한 실험입니다.
+
+## 어떻게 써볼수 있습니까?
+
+윈도우 샌드박스 환경에서 테스트해보는걸 권장합니다.
+
+- [WeditteuSetup.exe]() 를 다운로드해 설치합니다.
+- [sample.json.wdit](docs/samples/sample.json.wdit) 을 다운로드해 열어봅니다.
+
+`.json.wdit` 파일이 웹앱스타일의 브라우저에서 열리며 파일내부의 `$wdit` 에 명시된 웹페이지에서 편집할수 있게됩니다.
+
+## 한계
+
+브라우저를 제어하여 `<input>` 태그에 file을 주입하는 과정은 자동화 가능하나,
+`FileSystemHandle` 을 사용자대신 확보하는것에는 실패하여 파일로부터 앱을 열더라도 로컬파일에 직접적으로 접근하려면 `새 이름으로 저장`을 해야합니다.
